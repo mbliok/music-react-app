4c6b257bc10f6231801efd540a0c26d5a8562186
mbliok <mbliok@melontech.com>
2019-02-22 13:21:17 +0200

    semantic ui set up

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ class App extends React.Component {
 
   render() {
     return (
-      <div>
+      <div className="ui container">
         <Layout>
           <Switch>
             <Route exact path="/" component={HomePage} />
